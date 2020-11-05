67e90cfab32d7f682f014a789680e2c541a6d875
suraj9741 <surajjadhav9741@gmail.com>
2020-11-05 11:36:23 +0530

    Add : function for choose latter for user

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,10 +1,28 @@
 import java.util.*;
-public class Tic_Tac_Toe{	
-	public static void main(String args[]){
-		int Board_Size=10;
-		char []board=new char[Board_Size];
-		for(int i=1;i<Board_Size;i++){
+import java.util.Scanner;
+public class Tic_Tac_Toe{
+	static void EmptyBorad(){
+		//Add : Create a board array
+		int BoardSize=10;
+		char []board=new char[BoardSize];
+		for(int i=1;i<BoardSize;i++){
 			board[i]=' ';
 		}
+	}
+	
+	static void ChooseLetter(){	
+		Scanner MyObj = new Scanner(System.in);		
+		System.out.println("Choose the latter X or O");
+		String UserLatter = MyObj.nextLine();
+		if (UserLatter.equals("X")){
+			String ComputerLatter="O";
+		}
+		else{
+			String ComputerLatter="X";
+		}
+	}
+	public static void main(String args[]){
+		EmptyBorad();
+		ChooseLetter();
 	}
 }
