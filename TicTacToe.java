ccf0963dcbd60d85e228ffb6f5411d9d72e98e6f
suraj9741 <surajjadhav9741@gmail.com>
2020-11-05 14:22:36 +0530

    Change : Class name, methods, refactor code in UC 3

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,16 +1,28 @@
 import java.util.*;
 import java.util.Scanner;
-public class Tic_Tac_Toe{
-	static void EmptyBorad(){
+public class TicTacToe{
+	static void ticTacToeBorad(){
 		//UC 1 Add : Create a board array
 		int boardSize=10;
 		char []board=new char[boardSize];
 		for(int i=1;i<boardSize;i++){
 			board[i]=' ';
 		}
+		//UC 3 Add : function to create game board
+		char [][] gameBoard ={{board[1],'|',board[2],'|',board[3]},
+							  {'-','+','-','+','-'},
+							  {board[4],'|',board[5],'|',board[6]},
+							  {'-','+','-','+','-'},
+							  {board[7],'|',board[8],'|',board[9]}};
+		for (char[] row : gameBoard){
+			for (char column : row){
+				System.out.print(column);
+			}
+			System.out.println();
+		}
 	}
 	//UC 2 Add : function for choose latter for user
-	static void ChooseLetter(){	
+	static void chooseLetter(){	
 		Scanner MyObj = new Scanner(System.in);
 		while(true){
 			System.out.println("Choose the latter X or O");
@@ -28,24 +40,9 @@ public class Tic_Tac_Toe{
 			}
 		}
 	}
-	//UC 3 Add : function to create game board
-	static void GameBoard(){
-		char [][] gameBoard ={{' ','|',' ','|',' '},
-							  {'-','+','-','+','-'},
-							  {' ','|',' ','|',' '},
-							  {'-','+','-','+','-'},
-							  {' ','|',' ','|',' '}};
-		for (char[] row : gameBoard){
-			for (char column : row){
-				System.out.print(column);
-			}
-			System.out.println();
-		}
-	}
 	
 	public static void main(String args[]){
-		EmptyBorad();
-		ChooseLetter();
-		GameBoard();
+		chooseLetter();
+		ticTacToeBorad();
 	}
 }
