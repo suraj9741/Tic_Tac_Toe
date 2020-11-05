0b8f92569c0b290e73c0eff0180485385ea0d514
suraj9741 <surajjadhav9741@gmail.com>
2020-11-05 10:45:26 +0530

    Add : Create a board array

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,5 +1,10 @@
-public class Tic_Tac_Toe{
+import java.util.*;
+public class Tic_Tac_Toe{	
 	public static void main(String args[]){
-		System.out.println("Hello World");
+		int Board_Size=10;
+		char []board=new char[Board_Size];
+		for(int i=1;i<Board_Size;i++){
+			board[i]=' ';
+		}
 	}
 }
